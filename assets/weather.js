fc9b8cf15f61be04cb4ba04c8147c810c0cb4107
mackgabe <mackenzieabe@gmail.com>
2022-06-13 20:14:40 -0400

    added current-container and five-day container and got api working

--- assets/weather.js
+++ assets/weather.js
@@ -1,31 +1,57 @@
-//  current forecast endpoint - https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
-// 5day endpoint - https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={API key}
 
-const API_KEY = '1061cabb90305abccae90d57ba69481e'
-const searchBtn = document.querySelector('.searchButton')
+var cityName = document.querySelector('.cityName')
+var searchBtn = document.querySelector('.btn')
+var temp = document.querySelector('.temp')
+var fiveDayContainer = document.querySelector('.five-day-container')
 
-searchBtn.addEventListener('click', function() {
-    let city = document.getElementById('city').value
-    console.log(city);
-    getCurrent(city)
-})
+var getCurrentWeather= function(city) {
 
-function getCurrent(value) {
-   fetch('https://api.openweathermap.org/data/2.5/weather?q=' + value + '&appid=' + API_KEY + 'units=imperial')
-   .then( function (res) {
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+ city +'&appid=37c2c3166abd1655f69f9cfd6a5c6b2b&units=imperial')
+  .then(res => {
     return res.json()
-  }).then(function(data) {
-      console.log(data);
+  }).then(data => {
+    console.log(data);
 
-      
+    cityName.textContent = data.name
+    temp.textContent = data.main.temp + ' F'
 
-      let lat = data.coord.lat;
-      let lon = data.coord.lon;
-      getFiveDay(lat, lon)
+
+    var lat = data.coord.lat
+    var lon = data.coord.lon
+    getFiveDay(lat, lon)
   })
 }
 
-function getFiveDay(lat, lon) {
+var getFiveDay = function (lat, lon) {
+  fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+ lat + '&lon='+ lon + '&appid=37c2c3166abd1655f69f9cfd6a5c6b2b&units=imperial')
+  .then(res => {
+    return res.json()
+  })
+  .then(fiveDayData => {
+    console.log(fiveDayData);
 
+    for (var i = 1; i < 6; i++) {
+      var card = document.createElement('div')
+      card.classList.add('card')
+      fiveDayContainer.append(card)
+
+      var day = document.createElement('h1')
+      day.textContent = moment().add(i, 'days').format('dddd'); 
+      card.append(day)
+
+      var fiveTemp = document.createElement('p')
+      fiveTemp.textContent = fiveDayData.daily[i].temp.day + ' F'
+      card.append(fiveTemp)
+
+    }
+
+  })
 }
+
+searchBtn.addEventListener('click', function(e) {
+  e.preventDefault()
+   let city = document.getElementById('city-search').value
+    console.log(city);
+    getCurrentWeather(city)
+})
