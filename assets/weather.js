c2a4889fbc2f61a8a710176a0c7bccb3264e3c3d
mackgabe <mackenzieabe@gmail.com>
2022-06-20 08:35:14 -0400

    added current weather data and tried formatting cards

--- assets/weather.js
+++ assets/weather.js
@@ -3,7 +3,13 @@
 var cityName = document.querySelector('.cityName')
 var searchBtn = document.querySelector('.btn')
 var temp = document.querySelector('.temp')
+var wind= document.querySelector('.wind')
+var uvValue= document.querySelector('.uvi')
+var uvBadge= document.querySelector('.badge')
+//var image=document.querySelector('.image')
+var currentContainer=document.querySelector('.current-container')
 var fiveDayContainer = document.querySelector('.five-day-container')
+//var weatherIconZero= document.querySelector('.weather-icon-zero')
 
 var getCurrentWeather= function(city) {
 
@@ -13,11 +19,27 @@ var getCurrentWeather= function(city) {
   }).then(data => {
     console.log(data);
 
+    var card=document.createElement('div')
+    card.classList.add('card')
+    currentContainer.append(card)
+
     cityName.textContent = data.name
-    temp.textContent = data.main.temp + ' F'
+   currentContainer.append(cityName)
+    
+    var time=document.createElement('h3')
+    time.textContent=moment().add(10, 'days').calendar();
+    currentContainer.append(time);
+
+    temp.textContent = "Temp:  " + data.main.temp + ' F'
+    currentContainer.append(temp)
+
+    wind.textContent="Wind: " + data.wind.speed + " MPH"
+    currentContainer.append (wind)
+
+  
 
 
-    var lat = data.coord.lat
+   var lat = data.coord.lat
     var lon = data.coord.lon
     getFiveDay(lat, lon)
   })
@@ -33,7 +55,7 @@ var getFiveDay = function (lat, lon) {
 
     for (var i = 1; i < 6; i++) {
       var card = document.createElement('div')
-      card.classList.add('card')
+      card.classList.add('five-day-container')
       fiveDayContainer.append(card)
 
       var day = document.createElement('h1')
@@ -44,14 +66,47 @@ var getFiveDay = function (lat, lon) {
       fiveTemp.textContent = fiveDayData.daily[i].temp.day + ' F'
       card.append(fiveTemp)
 
+     uvValue.textContent="UV Index: " + fiveDayData.current.uvi 
+     currentContainer.append (uvValue)
+    // if (uvValue < 2) {
+    //    uvBadge.addClass("green");
+    //  }
+    //  else if (uvValue > 2 && uvIndex < 6) {
+    //    uvBadge.addClass("yellow");
+    //  }
+     
+     
+     
+     }
+    });
+    //   //Cannot figure out the icon--still working on it.
+    // var icon=data.weather[0].icon
+    // var iconUrlZero='https://openweathermap.org/img/wn/' + icon + '@2x.png'
+    // weatherIconZero.setAttribute('src', iconUrlZero)
+    // card.append(icon)
+      
+   
+
+      
+       
+      
+   
+
     }
 
-  })
-}
+  
+
 
 searchBtn.addEventListener('click', function(e) {
   e.preventDefault()
    let city = document.getElementById('city-search').value
-    console.log(city);
     getCurrentWeather(city)
-})
+localStorage.setItem('searchBtn',city)
+    // var divElement=document.getElementById('searched-city')
+    
+}) 
+// function showSavedCity() {
+//   var data=localStorage.getItem('searchBtn');
+//   var divElement= document.getElementsById('searched-city')
+//   }
+// showSavedCity();
